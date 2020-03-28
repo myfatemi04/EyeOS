347901adb79eb7f3dc71495bcbb1a95d6403690f
Michael Fatemi <myfatemi04@gmail.com>
2020-03-28 11:52:35 -0400

    Made main tracker thread

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import controller
+import time
+from tracker import *
+from threading import Thread
 
-controller.move_mouse(2000, 500)
+BLINK_WAIT = 1
 
-
+def run_tracker():
+    last_blink_time = 0
+    while True:
+        untransformed = get_pos()
+        transformed = get_transformed_pos(untransformed, None)
+        controller.move_mouse(*transformed)
+        
+        blink = is_blinking()
+        if blink:
+            blink_time = time.perf_counter()
+            if blink_time - last_blink_time < BLINK_WAIT:
+                controller.left_click()
+        
+eyeos_thread = Thread(target=run_tracker, name="EyeOS")
+eyeos_thread.start()
