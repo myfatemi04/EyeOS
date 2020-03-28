6a3fa8478f429e090c187fc9a4c2d50fb8d9c33f
Michael Fatemi <myfatemi04@gmail.com>
2020-03-28 14:05:20 -0400

    HOLY CRAP IT'S ACTUALLY WORKING

--- EyeTracker/controller.py
+++ EyeTracker/controller.py
@@ -1,7 +1,9 @@
 import pyautogui as pg
 
+pg.FAILSAFE = False
+
 def move_mouse(x, y):
-    pg.moveTo(x, y)
+    pg.moveTo(x, y, 0.05, pg.easeInOutQuad)
 
 def left_click():
     pg.leftClick()
--- EyeTracker/main.py
+++ EyeTracker/main.py
@@ -1,33 +1,117 @@
 import controller
 import time
-from tracker import *
 from threading import Thread
+
+import cv2
+from eye_tracking import EyeTracker
+
+# initialize the Eye Tracker
+tracker = EyeTracker()
+cap = cv2.VideoCapture(0)
 
 BLINK_WAIT = 1
 
-def calibrate():
-    print("Look at the upper-left corner")
-    upper_left = get_pos()
-    print("Look at the upper-right corner")
-    upper_right = get_pos()
-    print("Look at the bottom-right corner")
-    bottom_right = get_pos()
-    print("Look at the bottom-left corner")
-    bottom_left = get_pos()
-    return (upper_left, upper_right, bottom_right, bottom_left)
+def transform_pos(pos, left, right, top, bottom, screen_left, screen_right, screen_bottom, screen_top):
+    x, y = pos
+    width = right - left
+    screen_width = screen_right - screen_left
+    height = bottom - top
+    screen_height = screen_bottom - screen_top
+    
+    width_prop = (x - left)/width
+    height_prop = (y - top)/height
 
-def run_tracker():
+    return width_prop * screen_width + screen_left, height_prop * screen_height + screen_top
+
+def main_thread():
     last_blink_time = 0
+    last_frame_blinking = False
+    is_calibrated = False
+    top_left = None
+    bottom_right = None
+    msg_topleft = False
+    msg_bottomright = False
+
+    left = None
+    right = None
+    bottom = None
+    top = None
+
+    # top_left = True
+    # bottom_right = True
+
+    # left = -2.25
+    # top = -1.5
+
+    # right = 7.0
+    # bottom = 1.25
+
+    screen_left = 0
+    screen_top = 0
+    screen_right, screen_bottom = controller.pg.size()
+
+    last_valid_pos = None
     while True:
-        untransformed = get_pos()
-        transformed = get_transformed_pos(untransformed, None)
-        controller.move_mouse(*transformed)
-        
-        blink = is_blinking()
-        if blink:
-            blink_time = time.perf_counter()
-            if blink_time - last_blink_time < BLINK_WAIT:
-                controller.left_click()
-        
-eyeos_thread = Thread(target=run_tracker, name="EyeOS")
+        _, frame = cap.read()
+        tracker.refresh(frame)
+        frame = tracker.annotated_frame()
+        cv2.imshow("Tracker", frame)
+
+        is_blinking = tracker.is_blinking()
+        pos = tracker.get_average_offset()
+
+        if is_calibrated:
+            if is_blinking:
+                blink_time = time.perf_counter()
+                if not last_frame_blinking and blink_time - last_blink_time < BLINK_WAIT:
+                    pass # controller.left_click()
+                last_blink_time = blink_time
+            
+            if pos:
+                tpos = transform_pos(
+                    pos,
+                    left,
+                    right,
+                    top,
+                    bottom,
+                    screen_left,
+                    screen_right,
+                    screen_bottom,
+                    screen_top
+                )
+                print(f"{tpos[0]:.2f} {tpos[1]:.2f}", f"{pos[0] * 100:.2f} {pos[1] * 100:.2f}")
+                int_x = int(tpos[0])
+                int_y = int(tpos[1])
+                controller.move_mouse(int_x, int_y)
+        else:
+            ## Calibrate here
+            if not top_left:
+                if not msg_topleft:
+                    print("Look at the top left and blink")
+                    msg_topleft = True
+                if last_valid_pos and is_blinking and not last_frame_blinking:
+                    top_left = last_valid_pos
+                    print("Saved pos at ", top_left)
+                    left, top = top_left
+            elif not bottom_right:
+                if not msg_bottomright:
+                    print("Look at the bottom right and blink")
+                    msg_bottomright = True
+                if last_valid_pos and is_blinking and not last_frame_blinking:
+                    bottom_right = last_valid_pos
+                    print("Saved pos at ", bottom_right)
+                    right, bottom = bottom_right
+            else:
+                is_calibrated = True
+
+        if pos:            
+            last_valid_pos = pos
+
+        last_frame_blinking = is_blinking
+
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+    cap.release()
+
+eyeos_thread = Thread(target=main_thread, name="EyeOS")
 eyeos_thread.start()
