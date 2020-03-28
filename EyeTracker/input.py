d4de4fcafce51fce072b89409d5b73357927a488
Michael Fatemi <myfatemi04@gmail.com>
2020-03-28 12:32:00 -0400

    Added threads

--- EyeTracker/input.py
+++ EyeTracker/input.py
@@ -1,31 +1,30 @@
+import controller
+import time
+from threading import Thread
+
 import cv2
 from eye_tracking import EyeTracker
 
+# initialize the Eye Tracker
 tracker = EyeTracker()
 cap = cv2.VideoCapture(0)
 
-while True:
-    _, frame = cap.read()
-    tracker.refresh(frame)
-    frame = tracker.annotated_frame()
-    text = ""
+def main_thread():
+    while True:
+        _, frame = cap.read()
+        tracker.refresh(frame)
+        frame = tracker.annotated_frame()
+        text = ""
 
-    if tracker.is_blinking(): text = "Blinking"
-    elif tracker.is_right(): text = "Looking right"
-    elif tracker.is_left(): text = "Looking left"
-    elif tracker.is_center(): text = "Looking center"
+        if tracker.is_blinking(): text = "Blinking"
+        elif tracker.is_right(): text = "Looking right"
+        elif tracker.is_left(): text = "Looking left"
+        elif tracker.is_center(): text = "Looking center"
 
-    left = tracker.pupil_left_coords()
-    right = tracker.pupil_right_coords()
+        left = tracker.pupil_left_coords()
+        right = tracker.pupil_right_coords()
 
-    f = open("cocksucckler.txt", "w")
-    f.write(str(left) + "\t" + str(right) + "\t" + text + "\n")
-    f.close()
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
 
-
-
-    """Michael Do stuff here """
-
-
-
-    if cv2.waitKey(1) & 0xFF == ord('q'): break
+eyeos_thread = Thread(target=main_thread, name="EyeOS")
+eyeos_thread.start()
