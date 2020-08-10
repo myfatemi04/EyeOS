2589dd5882a0f12db416d41ea5513a2c0997513e
SuhasNandiraju <47066425+SuhasNandiraju@users.noreply.github.com>
2020-08-10 12:28:33 -0400

    Add files via upload

---
+++ New_EyeOS/markTest.py
@@ -0,0 +1,25 @@
+import cv2
+from eye_tracker import EyeTracker
+
+eyetracker = EyeTracker()
+webcam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = webcam.read()
+
+    if ret:
+        eyetracker.refresh(frame)
+
+        frame = eyetracker.annotated_frame()
+        text = ""
+
+
+        cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+        left_pupil = eyetracker.pupil_left_coords()
+        right_pupil = eyetracker.pupil_right_coords()
+
+    cv2.imshow("BruhFuckThis", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
