51d054381813373a2954d889934b9a5a09de4976
SuhasNandiraju <47066425+SuhasNandiraju@users.noreply.github.com>
2020-08-10 12:33:56 -0400

    Update pupil.py

--- New_EyeOS/eye_tracker/pupil.py
+++ New_EyeOS/eye_tracker/pupil.py
@@ -3,10 +3,6 @@ import cv2
 
 
 class Pupil(object):
-    """
-    This class detects the iris of an eye and estimates
-    the position of the pupil
-    """
 
     def __init__(self, eye_frame, threshold):
         self.iris_frame = None
@@ -18,15 +14,6 @@ class Pupil(object):
 
     @staticmethod
     def image_processing(eye_frame, threshold):
-        """Performs operations on the eye frame to isolate the iris
-
-        Arguments:
-            eye_frame (numpy.ndarray): Frame containing an eye and nothing else
-            threshold (int): Threshold value used to binarize the eye frame
-
-        Returns:
-            A frame with a single element representing the iris
-        """
         kernel = np.ones((3, 3), np.uint8)
         new_frame = cv2.bilateralFilter(eye_frame, 10, 15, 15)
         new_frame = cv2.erode(new_frame, kernel, iterations=3)
@@ -35,12 +22,6 @@ class Pupil(object):
         return new_frame
 
     def detect_iris(self, eye_frame):
-        """Detects the iris and estimates the position of the iris by
-        calculating the centroid.
-
-        Arguments:
-            eye_frame (numpy.ndarray): Frame containing an eye and nothing else
-        """
         self.iris_frame = self.image_processing(eye_frame, self.threshold)
 
         contours, _ = cv2.findContours(self.iris_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
